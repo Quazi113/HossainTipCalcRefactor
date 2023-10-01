49e9098c39f16302ecee93fbd29671b6ce5c729b
quazi <quazih2789@nycstudents.net>
2023-10-01 03:02:46 -0400

    Added print statement

--- src/TipCalculatorRunner.java
+++ src/TipCalculatorRunner.java
@@ -17,7 +17,8 @@ public class TipCalculatorRunner {
         System.out.println("Tip Percentage: " + bill.getTipPercentage() + "%");
         System.out.println("Total Tip: $" + String.format("%.2f", bill.tipAmount()));
         System.out.println("Total Bill With Tip: $" + String.format("%.2f", bill.totalBill()));
-        System.out.println("Per Person Cost Before Tip: $" + String.format("%.2f", bill.perPersonTipAmount()));
+        System.out.println("Per Person Cost Before Tip: $" + String.format("%.2f", bill.perPersonCostBeforeTip()));
+        System.out.println("Tip Per Person: $" + String.format("%.2f", bill.perPersonTipAmount()));
         System.out.println("Total Cost Per Person: $" + String.format("%.2f", bill.perPersonTotalCost()));
 
     }
